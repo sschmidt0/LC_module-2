af38eb1a61c669ff4e8f1ce39ae42b61a662f591
sakschmidt <sarah.schmidt@code.seat>
2023-12-01 18:48:59 +0100

    feat: add exercise 4

---
+++ exercise-set2.js
@@ -0,0 +1,24 @@
+/**
+ * EXERCISE 4 - Read Books
+ *
+ * Crea una función isBookRead que reciba una lista de libros y un título y devuelva si se ha leído o no dicho libro. Un libro es un objeto con title como string y isRead como booleano. En caso de no existir el libro devolver false TIP: Existe un método de Array.prototype que te ayudará a buscar según un patrón.
+ */
+var books = [
+    { title: 'Harry Potter y la piedra filosofal', isRead: true },
+    { title: 'Canción de hielo y fuego', isRead: false },
+    { title: 'Devastación', isRead: true },
+];
+var isBookRead = function (books, titleToSearch) {
+    if (books === void 0) { books = []; }
+    var searchBook = books.find(function (book) { return book.title === titleToSearch; });
+    if (searchBook === undefined) {
+        return false;
+    }
+    ;
+    return Boolean(searchBook.isRead);
+};
+console.log({ books: books });
+console.log("\u00ECs the book 'Devastaci\u00F3n' read?: ".concat(isBookRead(books, 'Devastación')));
+console.log("\u00ECs the book 'Canci\u00F3n de hielo y fuego' read?: ".concat(isBookRead(books, 'Canción de hielo y fuego')));
+console.log("\u00ECs the book 'Harry Potter y la piedra filosofal' read?: ".concat(isBookRead(books, 'Harry Potter y la piedra filosofal')));
+console.log("\u00ECs the book 'Software Engineering' read?: ".concat(isBookRead(books, 'Software Engineering')));
---
+++ exercise-set2.ts
@@ -0,0 +1,33 @@
+/**
+ * EXERCISE 4 - Read Books
+ *
+ * Crea una función isBookRead que reciba una lista de libros y un título y devuelva si se ha leído o no dicho libro. Un libro es un objeto con title como string y isRead como booleano. En caso de no existir el libro devolver false TIP: Existe un método de Array.prototype que te ayudará a buscar según un patrón.
+ */
+
+interface Book {
+  title: String;
+  isRead: Boolean;
+}
+
+const books: Book[] = [
+  { title: 'Harry Potter y la piedra filosofal', isRead: true },
+  { title: 'Canción de hielo y fuego', isRead: false },
+  { title: 'Devastación', isRead: true },
+];
+
+const isBookRead = (books: Book[] = [], titleToSearch: string): boolean => {
+  const searchBook = books.find((book) => book.title === titleToSearch);
+
+  if (searchBook === undefined) {
+    return false
+  };
+
+  return Boolean(searchBook.isRead);
+};
+
+console.log({ books });
+console.log(`ìs the book 'Devastación' read?: ${isBookRead(books, 'Devastación')}`);
+console.log(`ìs the book 'Canción de hielo y fuego' read?: ${isBookRead(books, 'Canción de hielo y fuego')}`);
+console.log(`ìs the book 'Harry Potter y la piedra filosofal' read?: ${isBookRead(books, 'Harry Potter y la piedra filosofal')}`);
+console.log(`ìs the book 'Software Engineering' read?: ${isBookRead(books, 'Software Engineering')}`);
+
